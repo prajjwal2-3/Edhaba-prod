282c0cc2b401dbc5a40e6b8e2a941c1398d750a2
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-01-24 06:56:35 +0530

    useState hook used to create filter

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,28 +2,32 @@ import Restcard from "./Rescard";
 import resdata from "../utils/mockdata";
 import { useState } from "react";
 
-const Body = ()=>{
-    return(
-       <div className="body">
-        <div className="filter">
-          <button className="btn-filter"onClick={()=>{console.log("button clicked")}}>Top rated restaurents</button>
-        </div>
-        <div className="restcontainer">
-        {resdata.restaurants.map((restaurant, index) => (
-                <Restcard key={index} resdata1={restaurant} />
-            ))}
-         </div>
-       </div> 
-    )
-}
+const Body = () => {
+  const [list, setList] = useState(resdata);
 
-//normal js variable
-
-let normal = something;
-
-//state variable
-
-const [normalarray] = useState();
-
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="btn-filter"
+          onClick={() => {
+            const newResdata = resdata.restaurants.filter(
+              (restaurants) => restaurants.info.avgRating > 4
+            );
+            setList({ ...list, restaurants: newResdata });
+            console.log(newResdata);
+          }}
+        >
+          Top rated restaurants
+        </button>
+      </div>
+      <div className="restcontainer">
+        {list.restaurants.map((restaurant, index) => (
+          <Restcard key={index} resdata1={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
