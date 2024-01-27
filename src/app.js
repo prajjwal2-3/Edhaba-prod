1fa9eca4495308e34354e7a954b5c59a4bfb21ed
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-01-27 22:12:31 +0530

    react router introduced

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import react from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Aboutpage from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Applayout = () => {
   return (
@@ -12,6 +14,17 @@ const Applayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Applayout />,
+  },
+  {
+    path: "/about",
+    element: <Aboutpage />,
+  },
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Applayout />);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+const Aboutpage = () => {
+    return(
+        <div>
+            <h1>about</h1>
+            <h2>this is namaste react series</h2>
+        </div>
+    )
+}
+export default Aboutpage;
