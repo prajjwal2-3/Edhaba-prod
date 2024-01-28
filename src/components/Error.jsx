bc6aa58c0c61c732a531076587eac4afc74fcc1e
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-01-28 21:40:30 +0530

    error information

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,6 +6,8 @@ const Error = ()=>{
         <div>
             <h1>OOPS!!!!</h1>
             <h2>something went wrong</h2>
+            <h3>Error {err.status}</h3>
+            <h3>{err.statusText}</h3>
         </div>
     )
 }
