2436aadfa763f6e3eafcca22606adeca848ea173
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-01-30 23:55:26 +0530

    menu api destructuring

--- src/components/Resmenu.jsx
+++ src/components/Resmenu.jsx
@@ -1,28 +1,42 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constants";
+import Shimmer from "./shimmer";
 
 const Resmenu = () => {
 const {resID} = useParams();
+const [menu, setmenu] = useState(null);
 
  useEffect(()=>{
         apicall();
         console.log("called")
-    })
+    },[])
 
     const apicall = async () => {
         try{
             const data = await fetch(MENU_API + resID)
             const json = await data.json();
             console.log(json)
+          setmenu(json.data)
         }
         catch(error){
          console.log(error)
         }
       };
+      if(menu === null) return <Shimmer />;
+
+      const {name,cuisines,city,locality,avgRatingString,isOpen} = menu.cards[0].card.card.info;
+      
     return(
+        
+        
         <div className="menu">
-            <h1 className="title">name of restaurant</h1>
+            <h1 className="title">{name}</h1>
+            <p>{cuisines.join("")}</p>
+            <p>{avgRatingString}* stars</p>
+            <p>{locality}</p>
+            <p>{city}</p>
+            <p>{isOpen}</p>
             <h2 className="menulist">
                 <ul>
                     <li>biryani</li>
