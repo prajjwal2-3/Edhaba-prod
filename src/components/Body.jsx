7b5ebdf96d05c8e544f8dea4696843cf798d2ebc
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-01-31 22:23:40 +0530

    formatted

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,11 +8,11 @@ const Body = () => {
   const [list, setList] = useState([]);
   const [searchtext, setsearchtext] = useState([]);
   const [isloading, setisloading] = useState(true);
-  const [afterfilter, setafterfilter] = useState([])
+  const [afterfilter, setafterfilter] = useState([]);
 
   useEffect(() => {
     fetchdata();
-    console.log("Api called")
+    console.log("Api called");
   }, []);
 
   const fetchdata = async () => {
@@ -23,18 +23,21 @@ const Body = () => {
 
       const json = await data.json();
 
-      console.log("below is api data ")
-            console.log(json)
+      console.log("below is api data ");
+      console.log(json);
       let dataIndex;
       for (let i = 1; i <= 4; i++) {
-        if (json.data.cards[i].card.card.hasOwnProperty('gridElements')) {
+        if (json.data.cards[i].card.card.hasOwnProperty("gridElements")) {
           dataIndex = i;
-          console.log("restaurants are found in index " + i)
-          break;  // Exit the loop once 'gridElements' is found
+          console.log("restaurants are found in index " + i);
+          break; // Exit the loop once 'gridElements' is found
         }
       }
       setList(json.data.cards[dataIndex].card.card.gridElements.infoWithStyle);
-      setafterfilter(json.data.cards[dataIndex].card.card.gridElements.infoWithStyle.restaurants);
+      setafterfilter(
+        json.data.cards[dataIndex].card.card.gridElements.infoWithStyle
+          .restaurants
+      );
       setisloading(false);
     } catch (error) {
       setisloading(false);
@@ -46,43 +49,52 @@ const Body = () => {
     const newResdata = list.restaurants.filter(
       (restaurant) => restaurant.info.avgRating > 4
     );
-    setList(list => ({ ...list, restaurants: newResdata }));
+    setList((list) => ({ ...list, restaurants: newResdata }));
     console.log(newResdata);
   };
 
-  const filterfunction = ()=>{
-  const filteredlist = list.restaurants.filter(
-        (res) => res.info.name.toLowerCase().includes(searchtext.toLowerCase())
-  );
-  setafterfilter(filteredlist);
-  console.log("search button pressed")
-  console.log(filteredlist)
-  }
+  const filterfunction = () => {
+    const filteredlist = list.restaurants.filter((res) =>
+      res.info.name.toLowerCase().includes(searchtext.toLowerCase())
+    );
+    setafterfilter(filteredlist);
+    console.log("search button pressed");
+    console.log(filteredlist);
+  };
 
   return isloading ? (
     <Shimmer />
   ) : (
     <div className="body">
       <div className="bars">
-      <div className="search">
-        <input type="text" className="search-input" value={searchtext} onChange={(e)=>{
-        setsearchtext(e.target.value);
-        }} />
-        <button onClick={filterfunction}>search</button>
-      </div>
-      <div className="filter">
-        <button className="btn-filter" onClick={handleFilter}>
-          Top rated restaurants
-        </button>
-      </div>
+        <div className="search">
+          <input
+            type="text"
+            className="search-input"
+            value={searchtext}
+            onChange={(e) => {
+              setsearchtext(e.target.value);
+            }}
+          />
+          <button onClick={filterfunction}>search</button>
+        </div>
+        <div className="filter">
+          <button className="btn-filter" onClick={handleFilter}>
+            Top rated restaurants
+          </button>
+        </div>
       </div>
 
       <div className="restcontainer">
         {afterfilter.map((res) => (
-          <Link to={"/restaurants/"+res.info.id} key={res.info.id} className="linkr"><Restcard   resdata1={res} /></Link>
-         
+          <Link
+            to={"/restaurants/" + res.info.id}
+            key={res.info.id}
+            className="linkr"
+          >
+            <Restcard resdata1={res} />
+          </Link>
         ))}
-      
       </div>
     </div>
   );
--- src/components/Resmenu.jsx
+++ src/components/Resmenu.jsx
@@ -1,73 +1,80 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constants";
 import Shimmer from "./shimmer";
 
 const Resmenu = () => {
-const {resID} = useParams();
-const [menu, setmenu] = useState(null);
+  const { resID } = useParams();
+  const [menu, setmenu] = useState(null);
 
- useEffect(()=>{
-        apicall();
-        console.log("Menu api called")
-    },[])
+  useEffect(() => {
+    apicall();
+    console.log("Menu api called");
+  }, []);
 
-    const apicall = async () => {
-        try{
-            const data = await fetch(MENU_API + resID)
-            const json = await data.json();
-            console.log("below is menu data ")
-            console.log(json)
-          setmenu(json.data)
-        }
-        catch(error){
-         console.log(error)
-        }
-      };
-      if(menu === null) return <Shimmer />;
+  const apicall = async () => {
+    try {
+      const data = await fetch(MENU_API + resID);
+      const json = await data.json();
+      console.log("below is menu data ");
+      console.log(json);
+      setmenu(json.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  if (menu === null) return <Shimmer />;
 
-      const {name,cuisines,city,locality,avgRatingString,isOpen} = menu.cards[0].card.card.info;
-      let dataIndex1;
-      let menufound =false;
-      for (let i = 0; i <= 4; i++) {
-        if (menu.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[i]?.card?.card?.hasOwnProperty('itemCards')) {
-          dataIndex1 = i;
-         menufound=true;
-          console.log("menu is found on index "+i)
-          break;  
-        }
-      }
-     if(!menufound){
-        console.log("menu not found")
-        return(
-            <>
-            <div>
-                <h1>no menu</h1>
-            </div>
-            </>
-        )
-     }
-      const menulist = menu.cards[2].groupedCard.cardGroupMap.REGULAR.cards[dataIndex1].card.card.itemCards;
-      const menunames= menulist.map(item => item.card.info)
-      console.log(menunames);
-    return(
-        
-        
-        <div className="menu">
-            <h1 className="title">{name}</h1>
-            <p>{cuisines.join("")}</p>
-            <p>{avgRatingString}* stars</p>
-            <p>{locality}</p>
-            <p>{city}</p>
-            <p>{isOpen}</p>
-            <h2 className="menulist">
-       <div>
-       {menunames.map((element, index) => (
-        <li key={index}>{element.name}-Rs {element.defaultPrice/100 || element.price/100}</li>
-      ))}
-       </div>
-            </h2>
+  const { name, cuisines, city, locality, avgRatingString, isOpen } =
+    menu.cards[0].card.card.info;
+  let dataIndex1;
+  let menufound = false;
+  for (let i = 0; i <= 4; i++) {
+    if (
+      menu.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[
+        i
+      ]?.card?.card?.hasOwnProperty("itemCards")
+    ) {
+      dataIndex1 = i;
+      menufound = true;
+      console.log("menu is found on index " + i);
+      break;
+    }
+  }
+  if (!menufound) {
+    console.log("menu not found");
+    return (
+      <>
+        <div>
+          <h1>no menu</h1>
         </div>
-    )
-}
+      </>
+    );
+  }
+  const menulist =
+    menu.cards[2].groupedCard.cardGroupMap.REGULAR.cards[dataIndex1].card.card
+      .itemCards;
+  const menunames = menulist.map((item) => item.card.info);
+  console.log(menunames);
+  return (
+    <div className="menu">
+      <h1 className="title">{name}</h1>
+      <p>{cuisines.join("")}</p>
+      <p>{avgRatingString}* stars</p>
+      <p>{locality}</p>
+      <p>{city}</p>
+      <p>{isOpen}</p>
+      <h2 className="menulist">
+        <div>
+          {menunames.map((element, index) => (
+            <li key={index}>
+              {element.name}-Rs{" "}
+              {element.defaultPrice / 100 || element.price / 100}
+            </li>
+          ))}
+        </div>
+      </h2>
+    </div>
+  );
+};
 export default Resmenu;
