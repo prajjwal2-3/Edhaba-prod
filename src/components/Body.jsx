b3de1c70e9e618be86d12e945d352606ec9a691e
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-08 22:13:11 +0530

    hurosx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -74,11 +74,11 @@ const Body = () => {
     
   ) : (
     <div className="body">
-      <div className="bars flex">
-        <div className="search ">
+      <div className="bars flex justify-center">
+        <div className="search p-2 m-4 ">
           <input
             type="text"
-            className="search-input"
+            className="search-input border-2 p-2 mx-4 rounded-lg border-gray-300 focus:border-gray-600"
             value={searchtext}
             onChange={(e) => {
               setsearchtext(e.target.value);
@@ -86,7 +86,7 @@ const Body = () => {
           />
           <button className="px-4 py-2 bg-green-400 rounded-lg" onClick={filterfunction}>search</button>
         </div>
-        <div className="filter p-2 m-4 ">
+        <div className="filter p-2 m-4">
           <button className=" px-4 py-2 bg-green-400 rounded-lg" onClick={handleFilter}>
             Top rated restaurants
           </button>
