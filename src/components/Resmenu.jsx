a5698c4fba346fe21af8c99f640a359dcbc71137
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-09 20:13:22 +0530

    menu page ui

--- src/components/Resmenu.jsx
+++ src/components/Resmenu.jsx
@@ -7,10 +7,17 @@ const Resmenu = () => {
   const menu = useResmenu(resID);
   if (menu === null) return <Shimmer />;
 
-  const { name, cuisines, city, locality, avgRatingString, isOpen, cloudinaryImageId } =
-    menu.cards[0].card.card.info;
+  const {
+    name,
+    cuisines,
+    city,
+    locality,
+    avgRatingString,
+    isOpen,
+    cloudinaryImageId,
+  } = menu.cards[0].card.card.info;
   let dataIndex1;
-  let menufound = false; 
+  let menufound = false;
   for (let i = 0; i <= 4; i++) {
     if (
       menu.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[
@@ -39,28 +46,43 @@ const Resmenu = () => {
   const menunames = menulist.map((item) => item.card.info);
   console.log(menunames);
   return (
-    <div className="menu  justify-center">
-      <div className="flex justify-center h-52 items-center bg-slate-600">
-      <div className="m-2 ">
-      <h1 className="font-extrabold text-white ">{name}</h1>
-      <p>{cuisines.join(", ")}</p>
-      <p>{avgRatingString}* stars</p>
-      <p>{locality}</p>
-      <p>{city}</p>
-      <p className="text-yellow-500">{isOpen}</p>
-      </div>
-      <img className="rounded-lg w-56 h-44" alt="image didnt load" src={
-             CDN_url  + cloudinaryImageId
-              }/>
-     
+    <div className="menu ">
+      <div className="flex justify-evenly  h-56 items-center bg-slate-600">
+        <div className="m-4 p-2 flex flex-col text-center">
+          <h1 className="font-extrabold text-4xl text-gray-300 ">{name}</h1>
+          <p>{cuisines.join(", ")}</p>
+          <p>{locality}</p>
+          <p>{city}</p>
+          <p>{avgRatingString}* stars</p>
+          <p>Restaurant: {isOpen.toString() ? "Open✅" : "Closed🔴"}</p>
+        </div>
+        <img
+          className="rounded-lg w-52 shadow-2xl  h-44"
+          alt="image didnt load"
+          src={CDN_url + cloudinaryImageId}
+        />
       </div>
       <h2 className="menulist">
-        <div>
+        <div className=" flex flex-col text-center">
           {menunames.map((element, index) => (
-            <li key={index}>
-              {element.name}-Rs{" "}
-              {element.defaultPrice / 100 || element.price / 100}
-            </li>
+            <div key={index} className="mx-28 m-6 p-4 rounded-md shadow-md ">
+              <div className="p-1 items-center flex justify-evenly">
+                <div className="">
+                  <p>
+                    {element.name}
+                  </p>
+                  <p>Rs{" "}
+                    {element.defaultPrice / 100 || element.price / 100}</p>
+                </div>
+                <div className="item-center">
+                  <img
+                    className="rounded-lg w-24  shadow-2xl"
+                    alt="image didnt load"
+                    src={CDN_url + element.imageId}
+                  />
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </h2>
