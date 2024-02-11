6be91cbfd3860d6cace0c0f87e95f6abf32c2da4
Prajjwal sharma <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-11 12:16:24 +0530

    menu api changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ export const CDN_url = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_url = "https://img.freepik.com/premium-vector/chef-food-restaurant-logo_7085-179.jpg"
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
+export const MENU_API = "https://foodfire.onrender.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
 
 export const RES_API = "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
