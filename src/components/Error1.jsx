d4f593bcddc1bc59c807cb3439ee55dd1dc5fe94
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-04 16:47:51 +0530

    mailto added

--- src/components/Error1.jsx
+++ src/components/Error1.jsx
@@ -4,9 +4,10 @@ const Error = ()=>{
     console.log(err)
     return(
         <div>
-            <h1>OOPS!!!!</h1>
-            <h2>something went wrong</h2>
-            <h3>Error {err.status}</h3>
+            <h1>Did you see something unexpected here maybe the API has changed yet another time.</h1>
+            <h2>If you can please inform me at</h2>
+            <p><a href="mailto:prajjwalbh25@gmail.com">Send email</a></p>
+            <h3> {err.status}</h3>
             <h3>{err.statusText}</h3>
         </div>
     )
